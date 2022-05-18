8523aaf7ec2dc241cd68f902f9c7c119bfa14e77
Sehajpreet Singh <ssehajpreet576@gmail.com>
2022-05-19 01:28:58 +0530

    initial commit

---
+++ code1.py
@@ -0,0 +1,34 @@
+from pickle import FALSE
+from flask import Flask
+from flask_restful import Api
+from flask_jwt import JWT
+from resources.user import UserRegister
+from db import db
+from resources.store import Store,StoreList
+
+
+from security import authenticate, identity
+from resources.item import Item, ItemList
+
+app=Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///data.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+app.secret_key = 'jose'
+api = Api(app)
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+jwt = JWT(app,authenticate,identity)  # /auth
+    
+api.add_resource(Item,'/item/<string:name>')
+api.add_resource(ItemList,'/items')
+api.add_resource(UserRegister,'/register')
+api.add_resource(Store,'/store/<string:name>')
+api.add_resource(StoreList,'/stores')
+
+if __name__=='__main__':
+    db.init_app(app)
+    print(__name__) # if we import code.py in other files it will run and app.run will also run, so to prevent it condition is needed
+    app.run(port=5000,debug=True)
